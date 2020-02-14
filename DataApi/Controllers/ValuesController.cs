8ed24426a8a92d2d1e087451f780d3234da80886
Jagadeesh <Jagadeesh.Musali@aa.com>
2020-02-13 21:32:02 -0700

    Add CORS

--- DataApi/Controllers/ValuesController.cs
+++ DataApi/Controllers/ValuesController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using DataApi.Models;
 
 namespace DataApi.Controllers
 {
     public class ValuesController : ApiController
     {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
 
         //http://localhost:51886/api/values
         //{
